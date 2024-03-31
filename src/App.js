e76a5bd6f85a323d54de3b65e3986db6b303b1e8
Tushar Agarwal <tamyworld@gmail.com>
2024-03-31 12:24:31 +0530

    no message

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
-import {useAuth} from "./contexts/AuthContext";
-import {Route, Router, Routes, redirect} from "react-router-dom";
+import {Route,BrowserRouter, Routes} from "react-router-dom";
 import Login from "./pages/login";
-import {ProtectedRoute} from "./components/ProtectedRoute";
 import TODO from "./pages/todo";
 import {AuthProvider} from "./contexts/AuthContext";
 
 function App() {
-const {authToken, updateToken} = useAuth();
-return <Router>
-        <AuthProvider>
-    <Routes>
-        <Route path="/login"><Login /></Route>
-        <ProtectedRoute path="/todo" component={TODO}/>
-        <Route exact path="/" render={()=>{
-            authToken? <redirect to="/todo"/>:<redirect to="/login"/>
-        }} />
-    </Routes>
-    </AuthProvider>
-</Router>
+return <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" exact element={<Login />} />
+          <Route path="/todo" element={<TODO />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
 }
 
 export default App;
