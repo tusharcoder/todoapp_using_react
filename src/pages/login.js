c3f564cd1380b54efb384cea9683c7209ec06d3e
Tushar Agarwal <tamyworld@gmail.com>
2024-03-31 12:24:49 +0530

    no message

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,20 +1,35 @@
 /*
 This is the login page
  */
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {useAuth} from "../contexts/AuthContext";
+import {useNavigate} from "react-router-dom";
 
 const Login =(props)=> {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const {updateToken} = useAuth();
+    const {authToken,updateAuth} = useAuth();
+    const  navigateTo = useNavigate();
+    useEffect(() => {
+        if(authToken){
+            navigateTo("/todo");
+        }
+    }, [authToken]);
+
+    const handleUsernameInput = (event)=>{
+            setUsername(event.target.value);
+        }
+    const handlePasswordInput = (event)=>{
+        setPassword(event.target.value);
+    }
     const login = () => {
-        updateToken(username);
+        updateAuth(username);
     };
+
     return (
       <div>
-          <input type="text" placeholder="Enter your username" value={username}/>
-          <input type="password" value={password}/>
+          <input type="text" placeholder="Enter your username" value={username} onChange={handleUsernameInput}/>
+          <input type="password" value={password} onChange={handlePasswordInput}/>
           <input type="button" value="Log In" onClick={login}/>
       </div>
     );
