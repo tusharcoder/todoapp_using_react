5b533deb22cb45293e0be7c45668e29008025167
Tushar Agarwal <tamyworld@gmail.com>
2024-03-31 12:45:14 +0530

    add styling to the login form

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,21 +5,21 @@ import {useEffect, useState} from "react";
 import {useAuth} from "../contexts/AuthContext";
 import {useNavigate} from "react-router-dom";
 
-const Login =(props)=> {
+const Login = (props) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const {authToken,updateAuth} = useAuth();
-    const  navigateTo = useNavigate();
+    const {authToken, updateAuth} = useAuth();
+    const navigateTo = useNavigate();
     useEffect(() => {
-        if(authToken){
+        if (authToken) {
             navigateTo("/todo");
         }
     }, [authToken]);
 
-    const handleUsernameInput = (event)=>{
-            setUsername(event.target.value);
-        }
-    const handlePasswordInput = (event)=>{
+    const handleUsernameInput = (event) => {
+        setUsername(event.target.value);
+    }
+    const handlePasswordInput = (event) => {
         setPassword(event.target.value);
     }
     const login = () => {
@@ -27,12 +27,26 @@ const Login =(props)=> {
     };
 
     return (
-      <div>
-          <input type="text" placeholder="Enter your username" value={username} onChange={handleUsernameInput}/>
-          <input type="password" value={password} onChange={handlePasswordInput}/>
-          <input type="button" value="Log In" onClick={login}/>
-      </div>
+        <div className={"container"}>
+            <div className={"loginForm"}>
+                <div className={"item heading"}>
+                    <h1>Login</h1>
+                </div>
+                <div className={"item"}>
+                    <label htmlFor="username">Username</label>
+                    <input id="username" type="text" placeholder="Enter your username" value={username}
+                           onChange={handleUsernameInput}/>
+                </div>
+                <div className={"item"}>
+                    <label htmlFor="password">Password</label>
+                    <input id="password" type="password" value={password} onChange={handlePasswordInput}/>
+                </div>
+                <div className={"item button"}>
+                    <input type="button" value="Log In" onClick={login}/>
+                </div>
+            </div>
+        </div>
     );
 }
 
-export default  Login;
+export default Login;
